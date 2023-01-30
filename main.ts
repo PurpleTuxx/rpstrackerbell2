67566c4ce37a0e4ba541a672d9199caff6d1401d
PurpleTuxx <122543467+PurpleTuxx@users.noreply.github.com>
2023-01-30 09:50:09 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+function Update_Scores () {
+    OLED.clear()
+    OLED.writeStringNewLine("Player 1: " + P1)
+    OLED.newLine()
+    OLED.writeStringNewLine("Player 2: " + P2)
+    OLED.newLine()
+    OLED.writeStringNewLine("Ties: " + Ties)
+    OLED.newLine()
+    OLED.writeStringNewLine("Rounds: " + Rounds)
+}
+let Rounds = 0
+let Ties = 0
+let P2 = 0
+let P1 = 0
+OLED.init(128, 64)
+OLED.writeStringNewLine("\"Ready to lose?\"")
+P1 = 0
+P2 = 0
+Ties = 0
+Rounds = 0
+basic.pause(2000)
+Update_Scores()
