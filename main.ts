7c31970b4b4503a93c57292487f3ab8b4ac0a9d3
PurpleTuxx <122543467+PurpleTuxx@users.noreply.github.com>
2023-01-31 13:10:31 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,6 +8,42 @@ function Update_Scores () {
     basic.pause(2000)
     Update_Scores()
 }
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        `)
+    basic.clearScreen()
+    P1 += 1
+    Rounds += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.clearScreen()
+    Ties += 1
+    Rounds += 1
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # # . .
+        . # . # .
+        . # # . .
+        . # . # .
+        . # # . .
+        `)
+    basic.clearScreen()
+    P2 += 1
+    Rounds += 1
+})
 input.onGesture(Gesture.Shake, function () {
     Update_Scores()
 })
